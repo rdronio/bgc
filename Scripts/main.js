44a89502b07898e19ba08edee7c14f4dfa118576
Roy Denver Ronio <rdenver0315@gmail.com>
2020-07-16 13:33:20 +0800

    added max height & scrollable table, added sticky headers on tableLayout

--- Scripts/main.js
+++ Scripts/main.js
@@ -208,6 +208,27 @@ $(".timepicker").each(function () {
   }
 });
 
+// Month Picker
+$(".monthPicker").each(function () {
+  try {
+    //Create jQueryUI datepicker
+    $(this).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      showButtonPanel: true,
+      dateFormat: "MM yy",
+      onClose: function (dateText, inst) {
+        $(this).datepicker(
+          "setDate",
+          new Date(inst.selectedYear, inst.selectedMonth, 1)
+        );
+      },
+    });
+  } catch (e) {
+    //console.log(e);
+  }
+});
+
 // Birthday Picker
 $(".bdayPicker").each(function () {
   try {
